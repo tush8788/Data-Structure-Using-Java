009c1c4478bd88290b7bd7374bfbfb34d2c81e9c
Tushar <tusharj8788@gmail.com>
2021-12-28 12:40:14 +0530

    updating array list

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
 class ArrayListDemo
 {
 	public static void main(String[] args) {
 		ArrayList<Integer> arr=new ArrayList<Integer>();
+		arr.add(10);
+		arr.add(20);
+		System.out.println(arr.get(1));   //return index 1 value 
+		System.out.println(arr.size());	//return how many element you are inserted in arr
 		
 	}
 }
