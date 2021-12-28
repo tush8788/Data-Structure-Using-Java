2baaa82e897df4f9e119c184896b38ef8f3a55ba
Tushar <tusharj8788@gmail.com>
2021-12-28 18:34:57 +0530

    creating function of all functions

--- ArrayListDemo.java
+++ ArrayListDemo.java
@@ -1,12 +1,76 @@
 import java.util.ArrayList;
 class ArrayListDemo
 {
+	static ArrayList<Integer> arr;
+	//Adding Value in Array List
+	static void addingElement(Integer i)
+	{
+		arr.add(i);
+	}
+
+	//Geting Value using IndexNumber
+	static void getingValue(Integer IndexNum)
+	{
+		System.out.println(arr.get(IndexNum));
+	}
+
+	//how many element you are inserted in ArrayList
+	static void getSize()
+	{
+		System.out.println(arr.size());
+	}
+
+	//Printing ArrayList Using foreach loop
+	static void printArrayList()
+	{
+		for(int i:arr)
+		{
+			System.out.print(i+" ");
+		}
+		System.out.println();
+	}
+
+	//Set value to given index (Replace)
+	static void setValue(int indexNum,int value)
+	{
+		arr.set(indexNum,value);
+	}
+
+	//remove element using indexNumber
+	static void removeValueIndex(int indexNum)
+	{
+		arr.remove(indexNum);
+		printArrayList();
+	}
+
+	//remove element using element 
+	static void removeValueValue(Integer i)
+	{
+		arr.remove(i);   //if value not found does not show any error 
+		printArrayList();
+	}
+
+	//ading value at given postion (not replace)
+	static void setValueUsingIndex(int indexNum,Integer val)
+	{
+		arr.add(indexNum,val);
+		printArrayList();
+	}
+
+	//Main Method 
 	public static void main(String[] args) {
-		ArrayList<Integer> arr=new ArrayList<Integer>();
-		arr.add(10);
-		arr.add(20);
-		System.out.println(arr.get(1));   //return index 1 value 
-		System.out.println(arr.size());	//return how many element you are inserted in arr
-		
+		arr=new ArrayList<Integer>();
+
+		addingElement(10);
+		addingElement(12);
+		addingElement(15);
+		getingValue(0);
+		printArrayList();
+		setValue(1,100);
+		printArrayList();
+		removeValueIndex(0);
+		Integer val=111;
+		removeValueValue(val);
+		setValueUsingIndex(0,121);
 	}
 }
